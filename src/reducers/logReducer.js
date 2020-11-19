813c488dd3467dbc47683d61aebf09be0f1b0321
Glenn Marshall-Adams <glenn.marshalladams@icloud.com>
2020-11-19 14:53:08 +0000

    implement Edit functions

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,5 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from "../actions/types";
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+} from "../actions/types";
 
 const initialState = {
   logs: null,
@@ -21,15 +30,35 @@ export default (state = initialState, action) => {
       return {
         ...state,
         logs: [...state.logs, action.payload],
-        loading: false
+        loading: false,
       };
     }
     case DELETE_LOG: {
       return {
         ...state,
-        logs: state.logs.filter(log => log.id !== action.payload),
-        loading: false
-      }
+        logs: state.logs.filter((log) => log.id !== action.payload),
+        loading: false,
+      };
+    }
+    case UPDATE_LOG: {
+      return {
+        ...state,
+        logs: state.logs.map((log) =>
+          log.id === action.payload.id ? action.payload : log
+        ),
+      };
+    }
+    case SET_CURRENT: {
+      return {
+        ...state,
+        current: action.payload,
+      };
+    }
+    case CLEAR_CURRENT: {
+      return {
+        ...state,
+        current: null,
+      };
     }
     case LOGS_ERROR: {
       console.error(action.payload);
